3f1dc2cb19faa23cc84b5118f66ed14d6c9caa89
niraj-mx07 <nirajmahajan61452@gmail.com>
2026-08-15 02:36:58 +0530

    the lint issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,30 @@
+const globals = require("globals");
+
 module.exports = [
     {
-        files: ["src/**/*.js", "tests/**/*.js"],
+        files: ["src/**/*.js"],
         languageOptions: {
             ecmaVersion: "latest",
             sourceType: "commonjs",
+            globals: {
+                ...globals.node,
+            },
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "error",
+            "no-console": "off",
+        },
+    },
+    {
+        files: ["tests/**/*.js"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "commonjs",
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
         },
         rules: {
             "no-unused-vars": "warn",
